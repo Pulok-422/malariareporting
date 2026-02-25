907fceaaa9b9218c6e5a5775be2d900f66d1ebce
Hasibul Ahmed Pulok <hasibulahmedpulok@gmail.com>
2026-02-25 11:10:35 +0600

    Create UserManagement.tsx

---
+++ src/components/UserManagement.tsx
@@ -0,0 +1,13 @@
+// src/components/UserManagement.tsx
+import React from 'react';
+
+const UserManagement = () => {
+  return (
+    <div>
+      <h2>User Management</h2>
+      <p>This is where user management would happen (e.g., create users, assign roles).</p>
+    </div>
+  );
+};
+
+export default UserManagement;
