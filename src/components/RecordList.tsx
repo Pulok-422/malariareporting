02267da84f176b8a0eb82c2ee0a4fccdbaf9c762
Hasibul Ahmed Pulok <hasibulahmedpulok@gmail.com>
2026-02-25 11:15:10 +0600

    Create RecordList.tsx

---
+++ src/components/RecordList.tsx
@@ -0,0 +1,14 @@
+// src/components/RecordList.tsx
+import React from "react";
+
+const RecordList = () => {
+  return (
+    <div>
+      <h2>Submitted Records</h2>
+      <p>This is where submitted records by SKs will be displayed.</p>
+      {/* Add further functionality like fetching and displaying records here */}
+    </div>
+  );
+};
+
+export default RecordList;
